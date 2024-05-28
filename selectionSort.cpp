ffd22b862506366e4cf1dbe71c156eab80fb0002
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-05-28 17:47:29 -0300

    Atualização das associações de arquivos C++ no arquivo de configuração do VS Code e adição de métricas de desempenho ao algoritmo Selection Sort

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,19 +1,31 @@
 #include "SelectionSort.h"
+#include <chrono>
 
 // O Selection Sort ordena a lista encontrando repetidamente o menor elemento da parte não ordenada
 // e colocando-o na posição correta na parte ordenada
-void selectionSort(std::vector<int>& arr) {
+Metrics selectionSort(std::vector<int>& arr) {
+    Metrics metrics = {0, 0, 0.0};
     int n = arr.size();
+    auto start = std::chrono::high_resolution_clock::now(); // Inicia a contagem do tempo
     // Percorre cada posição do array
     for (int i = 0; i < n - 1; ++i) {
         int minIdx = i;
         // Encontra o menor elemento na parte não ordenada
         for (int j = i + 1; j < n; ++j) {
+            metrics.comparisons++;
             if (arr[j] < arr[minIdx]) {
                 minIdx = j;
             }
         }
         // Troca o menor elemento encontrado com o primeiro elemento da parte não ordenada
-        std::swap(arr[i], arr[minIdx]);
+        if (minIdx != i) {
+            metrics.comparisons++;
+            std::swap(arr[i], arr[minIdx]);
+            metrics.movements++;
+        }
     }
+
+    auto end = std::chrono::high_resolution_clock::now(); // Finaliza a contagem do tempo
+    metrics.time_us = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count(); // Calcula o tempo de execução
+    return metrics;
 }
--- selectionSort.h
+++ selectionSort.h
@@ -2,11 +2,12 @@
 #define SELECTIONSORT_H
 
 #include <vector>
+#include "Metrics.h"
 
 // Função para realizar a ordenação usando o Selection Sort
 // Complexidade de tempo no pior caso: O(n^2)
 // Complexidade de tempo no melhor caso: O(n^2)
 // Melhor para: listas pequenas onde a estabilidade não é uma preocupação
-void selectionSort(std::vector<int>& arr);
+Metrics selectionSort(std::vector<int>& arr);
 
 #endif // SELECTIONSORT_H
